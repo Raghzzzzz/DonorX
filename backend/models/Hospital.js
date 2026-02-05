3d96d55daaac49db97888cc1d56c9b246df3220a
Ashwin Pranav M <ashwinpranav360@gmail.com>
2026-02-05 17:50:17 +0530

    req accepeted without map

--- backend/models/Hospital.js
+++ backend/models/Hospital.js
@@ -66,7 +66,7 @@ HospitalSchema.methods.matchPassword = async function (enteredPassword) {
 // Encrypt password using bcrypt
 HospitalSchema.pre('save', async function (next) {
     if (!this.isModified('password')) {
-        next();
+        
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
